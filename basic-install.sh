8d2e12de18fc7255c3e7f0555ef470df3d2996be
dudu.clx <dudu.clx@hotmail.fr>
2016-10-27 15:12:58 +0200

    Update basic-install.sh

--- basic-install.sh
+++ basic-install.sh
@@ -1,24 +1,22 @@
-### configuring path
+#!/bin/bash
 
+### configuring path
 GRAPHITE_HOME='/opt/graphite'
 GRAPHITE_CONF="${GRAPHITE_HOME}/conf"
 GRAPHITE_STORAGE="${GRAPHITE_HOME}/storage"
 GRAPHITE_SETTING="${GRAPHITE_HOME}/webapp/graphite"
 
 ### check existing installation
-
 if [[ -d $GRAPHITE_HOME ]]; then
   echo "Looks like you already have a Graphite installation in ${GRAPHITE_HOME}, aborting."
   exit 1
 fi
 
 ### update and install needed packages
-
 apt-get update -y
 apt-get install python-dev libcairo2-dev libffi-dev python-pip fontconfig apache2 libapache2-mod-wsgi git
 
 ### installing using github source
-
 git clone https://github.com/graphite-project/graphite-web.git
 git clone https://github.com/graphite-project/carbon.git
 git clone https://github.com/graphite-project/whisper.git
@@ -32,14 +30,12 @@ python setup.py install
 cd ..
 
 ### installing vhost conf for apache
-
 cp graphite.conf /etc/apache2/sites-available/graphite.conf
 
 ### set up a new database and create the initial schema
 PYTHONPATH=$GRAPHITE_HOME/webapp django-admin.py migrate --settings=graphite.settings --run-syncdb
 
 ### configuring graphite
-
 cp $GRAPHITE_CONF/graphite.wsgi.example $GRAPHITE_CONF/graphite.wsgi
 cp $GRAPHITE_CONF/carbon.conf.example $GRAPHITE_CONF/carbon.conf
 cp $GRAPHITE_CONF/dashboard.conf.example $GRAPHITE_CONF/dashboard.conf
@@ -67,7 +63,6 @@ a2enmod rewrite
 service apache2 reload
 
 ### installing grafana
-
 wget https://grafanarel.s3.amazonaws.com/builds/grafana_3.1.1-1470047149_amd64.deb
 apt-get install -y adduser libfontconfig
 dpkg -i grafana_3.1.1-1470047149_amd64.deb
@@ -75,6 +70,12 @@ dpkg -i grafana_3.1.1-1470047149_amd64.deb
 ### start grafana and make it running at boot
 service grafana-server start
 update-rc.d grafana-server defaults
+
+### restart apache
+/bin/systemctl reload
+/bin/systemctl enable grafana-server
+service apache2 reload
+service apache2 restart
 
 ### removing install tools
 #rm -R graphina
