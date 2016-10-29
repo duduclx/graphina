ac465760605619d48ec3671e14fc09cced08ef21
dudu.clx <dudu.clx@hotmail.fr>
2016-10-30 01:52:24 +0200

    Update dahdi.py

--- xivo/calls/conf/dahdi.py
+++ xivo/calls/conf/dahdi.py
@@ -4,6 +4,6 @@ ASTERISK_PORT = 5038
 ASTERISK_USER = "xivouser"
 ASTERISK_SECRET = "xivosecret"
 
-CARBON_HOST = "localhost"
+CARBON_HOST = "graphiteIP"
 CARBON_PORT = 2003
 DAHDI_METRIC = "xivo.host.asterisk.dahdi.max"
