9944374a4ca9e31165d5c6aa3cb7e47b601ae4c1
dudu.clx <dudu.clx@hotmail.fr>
2016-10-30 01:54:38 +0200

    Update install_cacmec.sh

--- xivo/calls/install_cacmec.sh
+++ xivo/calls/install_cacmec.sh
@@ -9,7 +9,7 @@ sed -e "s/xivoIP/$IP/g" conf/calls.py
 sed -e "s/xivoIP/$IP/g" conf/dahdi.py
 
 # getting xivouser
-echo "Please type your graphite server IP"
+echo "Please type your xivo user"
 echo "something like xivo"
 echo "======================="
 read -p "type here" USER
@@ -18,13 +18,22 @@ sed -e "s/xivouser/$IP/g" conf/calls.py
 sed -e "s/xivouser/$IP/g" conf/dahdi.py
 
 # getting xivosecret
-echo "Please type your graphite server IP"
+echo "Please type your xivo secret"
 echo "something like password"
 echo "======================="
 read -p "type here" SECRET
 
 sed -e "s/xivosecret/$IP/g" conf/calls.py
 sed -e "s/xivosecret/$IP/g" conf/dahdi.py
+
+# getting graphite ip
+echo "Please type your graphite server IP"
+echo "something like password"
+echo "======================="
+read -p "type here" GRAPHIP
+
+sed -e "s/graphiteIP/$GRAPHIP/g" conf/calls.py
+sed -e "s/graphiteIP/$GRAPHIP/g" conf/dahdi.py
 
 # running python script
 python calls.py
