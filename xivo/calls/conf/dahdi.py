ef5133806fe38c064db9761bf5269e3472751ad6
dudu.clx <dudu.clx@hotmail.fr>
2016-10-30 00:47:37 +0200

    Update and rename dhadi.py to dahdi.py

--- xivo/calls/conf/dahdi.py
+++ xivo/calls/conf/dahdi.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 ASTERISK_HOST = "xivoIP"
 ASTERISK_PORT = 5038
-ASTERISK_USER = "user"
-ASTERISK_SECRET = "secret"
+ASTERISK_USER = "xivouser"
+ASTERISK_SECRET = "xivosecret"
 
 CARBON_HOST = "localhost"
 CARBON_PORT = 2003
