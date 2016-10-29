fd0b482f1a0ffa3caf534f2d2d54da8fe7cbb2b7
dudu.clx <dudu.clx@hotmail.fr>
2016-10-30 00:26:34 +0200

    Update install-xivo-stats.sh

--- xivo/switchboard/install-xivo-stats.sh
+++ xivo/switchboard/install-xivo-stats.sh
@@ -13,13 +13,25 @@ sed 's/myserverIP/$IP/' conf/graphite.conf
 cp conf/amqp.conf /etc/collectd/collectd.conf.d
 cp conf/graphite.conf /etc/collectd/collectd.conf.d
 cp conf/001-custom.yml /etc/xivo-ctid-ng/conf.d/001-custom.yml
+#cp conf/network.conf /etc/collectd/collectd.conf.d/network.conf
+
+# getting the queue to monitor
+echo "Please type switboard or queue exten"
+echo "something like 1234"
+echo "======================="
+read -p "type here" EXTEN
+
+# editing switchboard_stats.conf
+sed -e "s/1234/$EXTEN/g" switchboard_stats.conf
+#or (if switchboard_stats.con is empty or more than one queue, may need loop)
+#cat example.conf | sed -e "s/1234/$EXTEN/g" >> switchboard_stats.conf
 
 # push dialplan with cp ??
-# cp dialplan/xivo_stats.conf /etc/xivo/asterisk/xivo_stats.conf
+# cp dialplan/switchboard_stats.conf /etc/xivo/asterisk/switchboard_stats.conf
 
 #run logstash docker
 docker run -p 25826:25826/udp -it --rm -v $(pwd):/config-dir logstash logstash -f /config-dir/logstash.conf
 
 # don't forget to
-echo "do not forget to create the dialplan !"
-echo "with the correct exten value, yeah"
+#echo "do not forget to create the dialplan !"
+#echo "with the correct exten value, yeah"
