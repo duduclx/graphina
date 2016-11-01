cf1bc9ba1cb259104f17c0102077bc4a4ede097f
dudu.clx <dudu.clx@hotmail.fr>
2016-11-01 18:19:55 +0100

    try some fixes
    have to test the script after some fixes

--- install/install_supervisor.sh
+++ install/install_supervisor.sh
@@ -15,10 +15,10 @@ fi
 
 ### update and install needed packages
 apt-get update -y
-apt-get install python-dev libcairo2-dev libffi-dev fontconfig apache2 libapache2-mod-wsgi
-apt-get install python-cairo python-django python-pip python-pyparsing python-memcache
+apt-get install -y python-dev libcairo2-dev libffi-dev fontconfig apache2 libapache2-mod-wsgi
+apt-get install -y python-cairo python-django python-pip python-pyparsing python-memcache
 #apt-get install python-django-tagging
-apt-get install uwsgi uwsgi-plugin-python
+apt-get install -y uwsgi uwsgi-plugin-python
 
 pip install pytz
 pip install 'django-tagging<0.4'
@@ -29,11 +29,11 @@ pip install carbon
 pip install graphite-web
 #pip install https://github.com/graphite-project/ceres/tarball/master
 
-### installing vhost conf for apache
-cp apache2/graphite.conf /etc/apache2/sites-available/graphite.conf
-
 ### set up a new database and create the initial schema
 #PYTHONPATH=$GRAPHITE_HOME/webapp django-admin.py migrate --settings=graphite.settings --run-syncdb
+
+# cleaning terminal
+clear
 
 ### configuring graphite
 cp $GRAPHITE_CONF/graphite.wsgi.example $GRAPHITE_CONF/graphite.wsgi
@@ -41,12 +41,26 @@ cp $GRAPHITE_CONF/carbon.conf.example $GRAPHITE_CONF/carbon.conf
 cp $GRAPHITE_CONF/dashboard.conf.example $GRAPHITE_CONF/dashboard.conf
 cp $GRAPHITE_CONF/graphTemplates.conf.example $GRAPHITE_CONF/graphTemplates.conf
 
-cp conf/storage-schemas.conf $GRAPHITE_CONF/storage-schemas.conf
-cp conf/storage-aggregation.conf $GRAPHITE_CONF/storage-aggregation.conf
-
 ### moving examples
 mkdir $GRAPHITE_CONF/examples
 mv $GRAPHITE_CONF/*.example $GRAPHITE_CONF/examples/
+
+### moving dir
+cd ../conf
+### installing configured carbon conf files
+cp carbon/storage-schemas.conf $GRAPHITE_CONF/storage-schemas.conf
+cp carbon/storage-aggregation.conf $GRAPHITE_CONF/storage-aggregation.conf
+### need minimalist local_settings
+### you may need to edit it for email and more stuff
+cp graphite/local_settings.py $GRAPHITE_SETTING/local_settings.py
+# editing mysecret for local_settings.py
+#cd ${GRAPHITE_HOME}/webapp/graphite
+# edit $GRAPHITE_SETTING/local_settings.py with generated secretkey
+# python manage.py generate_secret_key [--replace] [secretkey.txt]
+# SECRET=$( cat secretkey.txt )
+#sed -e "s/mysecretkey/$SECRET/g" $GRAPHITE_SETTING/local_settings.py
+### installing apache conf file
+cp apache2/graphite.conf /etc/apache2/sites-available/graphite.conf
 
 ### creating database
 # installing mysql
@@ -55,10 +69,9 @@ apt-get install mysql-server
 echo "Please type your mysql password for root user"
 echo "something like Mys3cr3t"
 echo "======================="
-read -p "type here" MYSQL
+read -p " type here " MYSQL
 
-# editing graphite.conf
-sed -e "s/myserverIP/$IP/g" ../conf/graphite.conf
+# create user and database for graphite
 mysql -u root -p$MYSQL << EOF
 create database graphite;
 create user 'graphite'@'localhost' identified by 'graphite_password';
@@ -68,40 +81,28 @@ EOF
 ### installing phpmyadmin
 apt-get install mcrypt
 service apache2 restart
-apt-get install phpmyadmin
-#Select “apache2.”
-#When asked to configure database for phpmyadmin with dbconfig-common, select yes
 
-### need minimalist local_settings
-### you may need to edit it for email and more stuff
-cp graphite/local_settings.py $GRAPHITE_SETTING/local_settings.py
+# cleaning terminal
+clear
+
+### instructions for phpmyadmin
+echo "Select apache2 with the spacebar.”
+echo "When asked to configure database for phpmyadmin with dbconfig-common,"
+echo "select yes."
+pause 10
+apt-get install phpmyadmin
 
 ### set pythonpath for django to run
 export PYTHONPATH=/usr/local/lib/python2.7/site-packages
 
 # Setup the Django database
 cd ${GRAPHITE_HOME}/webapp/graphite
-# edit $GRAPHITE_SETTING/local_settings.py with generated secretkey
-# python manage.py generate_secret_key [--replace] [secretkey.txt]
-# SECRET=$( cat secretkey.txt )
-# sed 's/mysecretkey/$SECRET/' $GRAPHITE_SETTING/local_settings.py
 python manage.py syncdb --noinput
 chown www-data:www-data ${GRAPHITE_STORAGE}/graphite.db
 chown -R www-data:www-data /opt/graphite/{storage,webapp}
 
 PYTHONPATH=$GRAPHITE_ROOT/webapp django-admin.py migrate --settings=graphite.settings --run-syncdb
 #/usr/lib/python2.7/site-packages/graphite/manage.py syncdb
-
-### run graphite
-#cd ${GRAPHITE_HOME}/webapp
-#./manage.py runserver
-#or
-#cd /opt/graphite/bin
-#sudo ./carbon-cache.py start
-#sudo su -c "./run-graphite-devel-server.py \
-#     /opt/graphite &> /var/log/graphite.log &"
-#or
-#carbon-daemon.py [options] <instance> <start|stop|status>
 
 ### installing grafana
 wget https://grafanarel.s3.amazonaws.com/builds/grafana_3.1.1-1470047149_amd64.deb
@@ -120,6 +121,12 @@ grafana-cli plugins install mtanda-histogram-panel
 wget https://dl.influxdata.com/influxdb/releases/influxdb_1.0.2_amd64.deb
 dpkg -i influxdb_1.0.2_amd64.deb
 ### see https://docs.influxdata.com/influxdb/v0.9/introduction/installation/ for config file
+# create influxdb database
+influx << EOF
+CREATE DATABASE cacti
+USE cacti
+DROP DATABASE _internal
+EOF
 
 ### installing elasticsearch
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.0.0.deb
@@ -149,4 +156,12 @@ update-rc.d influxdb defaults
 ### start elasticsearch and make it running at boot
 service elasticsearch start
 update-rc.d elasticsearch defaults 95 10
+sudo /etc/init.d/elasticsearch start
 
+### run graphite
+cd ${GRAPHITE_HOME}/bin
+./carbon-cache.py start
+#and
+cd ${GRAPHITE_HOME}/webapp/graphite
+chmod +x manage.py
+./manage.py runserver
