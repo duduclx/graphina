1093894de880314b01356a4367b993f14a5cccbf
dudu.clx <dudu.clx@hotmail.fr>
2016-11-02 15:00:22 +0100

    Delete dahdi.py

--- conf/calls/dahdi.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-ASTERISK_HOST = "xivoIP"
-ASTERISK_PORT = 5038
-ASTERISK_USER = "xivouser"
-ASTERISK_SECRET = "xivosecret"
-
-CARBON_HOST = "graphiteIP"
-CARBON_PORT = 2003
-DAHDI_METRIC = "xivo.host.asterisk.dahdi.max"
